04ac41f21dfafea155ee19e41b25051cacbfbe1e
harshas <hsenaratne@wabtec.com>
2021-08-10 23:36:53 +0530

    Todo List Add/Complete/Remove functions added

--- App.tsx
+++ App.tsx
@@ -1,21 +1,148 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
+interface IToDo {
+  text: string;
+  completed: boolean;
+}
 
 export default function App() {
+  const [value, setValue] = useState<string>("");
+  const [toDoList, setToDos] = useState<IToDo[]>([]);
+  const [error, showError] = useState<Boolean>(false);
+  const [checked, setChecked] = React.useState('first');
+
+  const handleSubmit = (): void => {
+    if (value.trim())
+      setToDos([...toDoList, { text: value, completed: false }]);
+    else showError(true);
+    setValue("");
+  };
+
+  const removeItem = (index: number): void => {
+    const newToDoList = [...toDoList];
+    newToDoList.splice(index, 1);
+    setToDos(newToDoList);
+  };
+
+  const toggleComplete = (index: number): void => {
+    const newToDoList = [...toDoList];
+    newToDoList[index].completed = !newToDoList[index].completed;
+    setToDos(newToDoList);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Cabbage Todo</Text>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          placeholder="Enter your todo task..."
+          value={value}
+          onChangeText={e => {
+            setValue(e);
+            showError(false);
+          }}
+          style={styles.inputBox}
+        />
+        <Button title="Add Task" onPress={handleSubmit} />
+      </View>
+      {error && (
+        <Text style={styles.error}>Error: Input field is empty...</Text>
+      )}
+      <Text style={styles.subtitle}>Tasks</Text>
+      {toDoList.length === 0 && <Text>No to do task available</Text>}
+      {toDoList.map((toDo: IToDo, index: number) => (
+        <View style={styles.listItem} key={`${index}_${toDo.text}`}>
+          <Text
+            style={[
+              styles.task,
+              { textDecorationLine: toDo.completed ? "line-through" : "none" }
+            ]}
+          >
+            {toDo.text}
+          </Text>
+
+        <View style={{backgroundColor:'yellow'}}>
+          <Button 
+            title={toDo.completed ? "Completed" : "Complete"}
+            onPress={() => toggleComplete(index)}
+          />
+        </View>
+       
+        <View style={styles.iconContainer}>
+
+      <Ionicons name="md-trash-outline" size={32} color="black" onPress={()=>{removeItem(index)}}/>
+    </View>
+          {/* <Button
+            title="X"
+            onPress={() => {
+              removeItem(index);
+            }}
+            color="crimson"
+          /> */}
+        </View>
+      ))}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 35,
+    alignItems: "center"
   },
+  iconContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputWrapper: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20
+  },
+  inputBox: {
+    width: 200,
+    borderColor: "purple",
+    borderRadius: 8,
+    borderWidth: 2,
+    paddingLeft: 8
+  },
+  title: {
+    fontSize: 40,
+    marginBottom: 40,
+    fontWeight: "bold",
+    textDecorationLine: "underline"
+  },
+  subtitle: {
+    fontSize: 20,
+    marginBottom: 20,
+    color: "purple"
+  },
+  listItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+    backgroundColor: '#68a0cf',
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: '#fff', 
+   },
+  addButton: {
+    alignItems: "flex-end"
+  },
+  task: {
+    width: 200
+  },
+  error: {
+    color: "red"
+  }
 });
---
+++ components/card/styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: Colors.white,
+    },
+    mainCardView: {
+      height: 90,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: Colors.white,
+      borderRadius: 15,
+      shadowColor: Colors.shadow,
+      shadowOffset: {width: 0, height: 0},
+      shadowOpacity: 1,
+      shadowRadius: 8,
+      elevation: 8,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingLeft: 16,
+      paddingRight: 14,
+      marginTop: 6,
+      marginBottom: 6,
+      marginLeft: 16,
+      marginRight: 16,
+    },
+    subCardView: {
+      height: 50,
+      width: 50,
+      borderRadius: 25,
+      backgroundColor: Colors.history_back,
+      borderColor: Colors.color_eeeeee,
+      borderWidth: 1,
+      borderStyle: 'solid',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
